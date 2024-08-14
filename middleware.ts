c92fbcfb2058c0f6714f7bb017b22da9866927bd
tushcmd <muturidavid854@gmail.com>
2024-08-15 00:05:58 +0300

    feat: new favicon & protecting routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isPublicRoute = createRouteMatcher(["/signin(.*)", "/signup(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/flashcards(.*)",
+  "/generate(.*)",
+  "/generate-pro(.*)",
+]);
 
-export default clerkMiddleware((auth, request) => {
-   // if (!isPublicRoute(request)) {
-   //   auth().protect();
-   // }
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
 });
-
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
